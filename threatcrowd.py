0ef2dfc0c24ea7af614ac42e4c4b75076a54ce1b
Numb Shiva <nmbshiva@users.noreply.github.com>
2020-05-27 15:34:43 +0930

    Update threatcrowd.py
    updated for python3

--- threatcrowd.py
+++ threatcrowd.py
@@ -1,50 +1,40 @@
-#!/bin/env python
-
-instr = "\n[=] Queries threatcrowd.org for domain provided." \
-	"\n[=] Pretty prints json response." \
-	"\n[=] Writes response to file in same directory as script." \
-	"\n"
+#python3
 
 import requests
 import json
 import sys
 from datetime import datetime
 
-print instr
-
 if len(sys.argv) < 2:
 	sys.exit("[!] Error! No domain specified!\n")
 
-f = open(sys.argv[1]+'.txt', 'w')
-log = open('log.txt', 'w')
-i = datetime.now()
+f=open(sys.argv[1]+'.txt', 'w')
+log=open('log.txt', 'w')
+i=datetime.now()
 
-print "[+] Started at {0}".format(i.strftime("%H:%M:%S %d/%m/%Y"))
-print "[+] Querying domain {0}, please wait.\n".format(sys.argv[1])
+print("\nQuerying threatcrowd to find list of subdomains\n")
+print("[+] Started at {0}".format(i.strftime("%H:%M:%S %d/%m/%Y")))
+print("[+] Querying domain {0}, please wait.\n".format(sys.argv[1]))
 
 try:
 	
-	result =  requests.get("http://www.threatcrowd.org/searchApi/v2/domain/report/", \
-	params = {"domain": sys.argv[1]})
-
-	data = json.loads(result.text)
-	print "[+] Written to file {0}.txt.".format(sys.argv[1])
-	urls = json.loads(result.text)
-	print "[+] Subdomains found:\n"
-#	f.write("[+] Subdomains found:\n")
+	result=requests.get("http://www.threatcrowd.org/searchApi/v2/domain/report/", \
+	params={"domain": sys.argv[1]})
+	data=json.loads(result.text)
+	print("[+] Written to file {0}.txt.".format(sys.argv[1]))
+	urls=json.loads(result.text)
+	print("[+] Subdomains found:\n")
 	for url in urls["subdomains"]:
-		print "	-> {0}".format(url)
+		print("	-> {0}".format(url))
 		f.write(url + "\n")
-	print "\n"
+	print("\n")
 	log.write("\n<!-- "+ i.strftime("%H:%M:%S %d/%m/%Y") +" -->\n")
 	log.write(json.dumps(data, indent=4, sort_keys=True))
 	log.write("\n")
-
 	f.close()
 	log.close()
 
-except Exception,e:
-	print e
-#	print "[!] Oops! Something went wrong!\n"
+except Exception as e:
+	print(e)
 	f.close()
 	sys.exit(0)
